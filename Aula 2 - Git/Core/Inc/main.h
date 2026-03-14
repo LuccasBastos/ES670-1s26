c57508967e4f5874dc98214c72147af3cf80af8e
Luccas Bastos <62710085+LuccasBastos@users.noreply.github.com>
2026-03-14 01:59:32 -0300

    Aula 2 - 1 Atividade

---
+++ Aula 2 - Git/Core/Inc/main.h
@@ -0,0 +1,107 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define B1_EXTI_IRQn EXTI15_10_IRQn
+#define LPUART1_TX_Pin GPIO_PIN_2
+#define LPUART1_TX_GPIO_Port GPIOA
+#define LPUART1_RX_Pin GPIO_PIN_3
+#define LPUART1_RX_GPIO_Port GPIOA
+#define LED_Y_Pin GPIO_PIN_5
+#define LED_Y_GPIO_Port GPIOA
+#define BT_Enter_Pin GPIO_PIN_5
+#define BT_Enter_GPIO_Port GPIOC
+#define BT_Enter_EXTI_IRQn EXTI9_5_IRQn
+#define BT_Baixo_Pin GPIO_PIN_7
+#define BT_Baixo_GPIO_Port GPIOC
+#define BT_Baixo_EXTI_IRQn EXTI9_5_IRQn
+#define BT_Esq_Pin GPIO_PIN_8
+#define BT_Esq_GPIO_Port GPIOC
+#define BT_Esq_EXTI_IRQn EXTI9_5_IRQn
+#define BT_Dir_Pin GPIO_PIN_9
+#define BT_Dir_GPIO_Port GPIOC
+#define BT_Dir_EXTI_IRQn EXTI9_5_IRQn
+#define LED_R_Pin GPIO_PIN_11
+#define LED_R_GPIO_Port GPIOA
+#define LED_G_Pin GPIO_PIN_12
+#define LED_G_GPIO_Port GPIOA
+#define T_SWDIO_Pin GPIO_PIN_13
+#define T_SWDIO_GPIO_Port GPIOA
+#define T_SWCLK_Pin GPIO_PIN_14
+#define T_SWCLK_GPIO_Port GPIOA
+#define Buzzer_Pin GPIO_PIN_15
+#define Buzzer_GPIO_Port GPIOA
+#define T_SWO_Pin GPIO_PIN_3
+#define T_SWO_GPIO_Port GPIOB
+#define BT_Cima_Pin GPIO_PIN_6
+#define BT_Cima_GPIO_Port GPIOB
+#define BT_Cima_EXTI_IRQn EXTI9_5_IRQn
+#define LED_B_Pin GPIO_PIN_8
+#define LED_B_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
