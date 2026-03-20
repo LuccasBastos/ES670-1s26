fa4acb500b3d2b1b9b89b0be8860274d64413da2
João Vitor Roque <mrjoao2001@gmail.com>
2026-03-19 21:24:17 -0300

    Add files via upload

--- Aula 6 - Git/Core/Inc/communicationStateMachine.h
+++ Aula 6 - Git/Core/Inc/communicationStateMachine.h
@@ -1,17 +1,17 @@
-/*
+/**
 ******************************************************************************
 * @file    : communicationStateMachine.h
 * @brief   : Cabecalho da maquina de estados de comunicacao UART
 ******************************************************************************
 * @author  : Luccas Bastos e Joao Vitor Roque
 * @date    : 12-Mar-2026
-* @version : 16-Mar-2026
+* @version : 19-Mar-2026
 ******************************************************************************
 */
 
-#ifndef SRC_COMMUNICATIONSTATEMACHINE_H_
-#define SRC_COMMUNICATIONSTATEMACHINE_H_
+#ifndef COMMUNICATIONSTATEMACHINE_H
+#define COMMUNICATIONSTATEMACHINE_H
 
-void CommunicationStateMachineProcessByteCommunication(unsigned char ucByte);
+void vCommunicationStateMachineProcessByteCommunication(unsigned char ucByte);
 
-#endif /* SRC_COMMUNICATIONSTATEMACHINE_H_ */
+#endif
--- Aula 6 - Git/Core/Inc/main.h
+++ Aula 6 - Git/Core/Inc/main.h
@@ -1,28 +1,14 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * This file contains functions needed to perform as a coffee machine. It makes
-  * LEDs blink, Buzzer sounds and it has buttons to select drinks options.
-  *
-  *@author  : João Vitor Roque Ribeiro & Luccas Pereira Bastos
-  *@date    : 06-Mar-2026
-  *@version : 1.0
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+******************************************************************************
+* @file    : main.h
+* @brief   : Cabecalho principal do programa da maquina de cafe
+******************************************************************************
+* @author  : Luccas Bastos e Joao Vitor Roque
+* @date    : 12-Mar-2026
+* @version : 19-Mar-2026
+******************************************************************************
+*/
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
